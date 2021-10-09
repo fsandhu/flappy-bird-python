314a1c7dc9620993a0aa8aca66c4a8632ef60e94
Fateh Sandhu <fatehkaran@huskers.unl.edu>
2021-10-08 19:17:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pygame
 import sys
 
+
 def draw_floor():
     screen.blit(floor_surface, (floor_x_pos, int(900 / 2)))
-    screen.blit(floor_surface, (floor_x_pos + int(576/2), int(900 / 2)))
+    screen.blit(floor_surface, (floor_x_pos + int(576 / 2), int(900 / 2)))
+
 
 pygame.init()
-screen = pygame.display.set_mode((int(576/2), int(1024/2)))
-clock = pygame.time.Clock() # sets frame rate
+
+screen = pygame.display.set_mode((int(576 / 2), int(1024 / 2)))
+clock = pygame.time.Clock()  # sets frame rate
 
 bg_surface = pygame.image.load('assets/background-day.png').convert()
 floor_surface = pygame.image.load('assets/base.png').convert()
 floor_x_pos = 0
+
+bird_surface = pygame.image.load('assets/bluebird-midflap.png').convert()
+bird_rect = bird_surface.get_rect(center=(int(100 / 2), int(512 / 2)))  # rects detect collisions
 
 while True:
     for event in pygame.event.get():
@@ -20,9 +26,10 @@ while True:
             sys.exit()
 
     screen.blit(bg_surface, (0, 0))
+    screen.blit(bird_surface, bird_rect)
     floor_x_pos -= 1
     draw_floor()
-    if floor_x_pos <= -int(576/2):
+    if floor_x_pos <= -int(576 / 2):
         floor_x_pos = 0
 
     pygame.display.update()
