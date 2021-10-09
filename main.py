0fe50e85176ed144eda79c81f91cfe6f323a919d
Fateh Sandhu <fatehkaran@huskers.unl.edu>
2021-10-09 03:23:40 -0500

    added sounds and score

--- main.py
+++ main.py
@@ -33,9 +33,11 @@ def draw_pipes(pipes):
 def check_collision(pipes):
     for pipe in pipes:
         if bird_rect.colliderect(pipe):
+            death_sound.play()
             return False
 
     if bird_rect.top <= -50 or bird_rect.bottom >= int(900 / 2):
+        death_sound.play()
         return False
 
     return True
@@ -45,29 +47,49 @@ def rotate_bird(bird):
     new_bird = pygame.transform.rotozoom(bird, -(bird_movement * 4), 1)
     return new_bird
 
+
 def bird_animation():
     new_bird = bird_frames[bird_index]
-    new_bird_rect = new_bird.get_rect(center=(int(100/2), bird_rect.centery))
+    new_bird_rect = new_bird.get_rect(center=(int(100 / 2), bird_rect.centery))
     return new_bird, new_bird_rect
 
+
+def score_display(game_state):
+    if game_state == 'main_game':
+        score_surface = game_font.render(str(int(score)), True, (255, 255, 255))
+        score_rect = score_surface.get_rect(center=(144, 50))
+        screen.blit(score_surface, score_rect)
+    if game_state == 'game_over':
+        score_surface = game_font.render(f'Score: {int(score)}', True, (255, 255, 255))
+        score_rect = score_surface.get_rect(center=(144, 50))
+        screen.blit(score_surface, score_rect)
+        high_score_surface = game_font.render(f'High Score: {int(high_score)}', True, (255, 255, 255))
+        high_score_rect = score_surface.get_rect(center=(122, 25))
+        screen.blit(high_score_surface, high_score_rect)
+
+
+pygame.mixer.pre_init(frequency=44100, size=16, channels=2, buffer=512)
 pygame.init()
 
 screen = pygame.display.set_mode((int(576 / 2), int(1024 / 2)))
 clock = pygame.time.Clock()  # sets frame rate
+game_font = pygame.font.Font('04B_19.ttf', 20)
 
 # game variables
 gravity = 0.2
 bird_movement = 0
 game_active = True
+score = 0
+high_score = 0
 
 # surfaces
-bg_surface = pygame.image.load('assets/background-day.png').convert()
+bg_surface = pygame.image.load('assets/background-night.png').convert()
 floor_surface = pygame.image.load('assets/base.png').convert()
 floor_x_pos = 0
 
-bird_up = pygame.image.load('assets/redbird-upflap.png').convert_alpha()
-bird_mid = pygame.image.load('assets/redbird-midflap.png').convert_alpha()
-bird_down = pygame.image.load('assets/redbird-downflap.png').convert_alpha()
+bird_up = pygame.image.load('assets/bluebird-upflap.png').convert_alpha()
+bird_mid = pygame.image.load('assets/bluebird-midflap.png').convert_alpha()
+bird_down = pygame.image.load('assets/bluebird-downflap.png').convert_alpha()
 bird_frames = [bird_down, bird_mid, bird_up]  # animates the bird
 bird_index = 0
 bird_surface = bird_frames[bird_index]
@@ -82,6 +104,14 @@ SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE, 1200)
 pipe_height = [200, 300, 400]
 
+game_over_surface = pygame.image.load('assets/gameover.png').convert_alpha()
+game_over_rect = game_over_surface.get_rect(center=(144, 230))
+
+flap_sound = pygame.mixer.Sound('sound/sfx_wing.wav')
+death_sound = pygame.mixer.Sound('sound/sfx_hit.wav')
+score_sound = pygame.mixer.Sound('sound/sfx_point.wav')
+score_countdown = 100
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -91,11 +121,13 @@ while True:
             if event.key == pygame.K_SPACE and game_active:
                 bird_movement = 0
                 bird_movement -= 4
+                flap_sound.play()
             if event.key == pygame.K_SPACE and game_active is False:
                 game_active = True
                 pipe_list.clear()
                 bird_rect.center = (int(100 / 2), int(512 / 2))
                 bird_movement = 0
+                score = 0
         if event.type == SPAWNPIPE:
             pipe_list.extend(create_pipe())
         if event.type == BIRDFLAP:
@@ -116,6 +148,16 @@ while True:
 
         pipe_list = move_pipes(pipe_list)
         draw_pipes(pipe_list)
+        score += 0.01
+        score_display("main_game")
+    else:
+        screen.blit(game_over_surface, game_over_rect)
+        game_over_text = game_font.render("Tap to play again", True, (255, 255, 255))
+        game_over_text_rect = game_over_text.get_rect(center=(144, 270))
+        screen.blit(game_over_text, game_over_text_rect)
+        if score > high_score:
+            high_score = score
+        score_display("game_over")
 
     floor_x_pos -= 1
     draw_floor()
